16cea9b4bd2bce4d3ed162e8b1846885611d1453
Michael Dors <115369454+MichaelDors@users.noreply.github.com>
2025-07-13 21:42:57 +0000

    lab2

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8883f85a28c821e7306f0a511fd607eb38168d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8720d7021058dc1af0394790070ecb994e45a74")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
